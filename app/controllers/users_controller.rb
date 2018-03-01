8ff51d06d28818e6c3aa45053050fbfcc8afbba8
Srishti Sehtia <srishti0123@gmail.com>
2018-03-01 09:34:29 -0800

    update users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,27 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      redirect_to @user
+    else
+      flash[:error] = 'Error'
+      redirect_to user_new_path
+    end
   end
 
   def show
+    user_id = params[:id]
+    @user = User.find_by_id(user_params)
   end
 
   def edit
@@ -19,6 +30,12 @@ class UsersController < ApplicationController
 
   def update
 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 
 end
