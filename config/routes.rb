df77307d92e1dbab82c631d2bab338047f3bb4df
Srishti Sehtia <srishti0123@gmail.com>
2018-03-01 10:47:48 -0800

    create questions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
   get '/login', to: "sessions#new"
   post '/sessions', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
   # root 'main#home'
-  resources :users
+  resources :users do
+    resources :questions
+  end
 end
