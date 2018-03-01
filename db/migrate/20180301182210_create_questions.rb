7a75710a266dbe0c5464097e02594d2eda114dab
Srishti Sehtia <srishti0123@gmail.com>
2018-03-01 10:26:49 -0800

    create questions model and association to user model

---
+++ db/migrate/20180301182210_create_questions.rb
@@ -0,0 +1,11 @@
+class CreateQuestions < ActiveRecord::Migration[5.1]
+  def change
+    create_table :questions do |t|
+      t.string :question
+      t.string :answer
+      t.belongs_to :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
