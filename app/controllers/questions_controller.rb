fdb4a30fb3b087c309d08094dc222b7f83372467
marcellino-ornelas <marcellino.ornelas@gmail.com>
2018-03-01 16:54:24 -0800

    questions controller is now working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+
   def index
   end
 
@@ -6,11 +7,25 @@ class QuestionsController < ApplicationController
   end
 
   def create
+    @question = Question.new(question_params)
+    @question.user = User.find(params[:user_id])
+    if @question.save
+      flash[:notice] = "You question has been submitted"
+    else
+      flash[:error] = "There was a error with uploading your question. Please try again later"
+    end
+      redirect_to user_path(@question.user)
+    # p @question
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+  def question_params
+    params.require(:questions).permit(:question)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   has_secure_password
+  has_many :questions
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
